fb74ead93544aca49e7af0d1171ba1a02685f4fa
Osi <adeokhorj@gmail.com>
2026-05-07 12:39:15 +0100

    update requirements

--- appraisal_system/settings.py
+++ appraisal_system/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'appraisal_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+         'DIRS': ['D:/apparaisal_system/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
