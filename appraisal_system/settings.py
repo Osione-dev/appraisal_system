eba146b0909e1c493802808e943dda121484223a
Joseph <adeokhorj@gmail.com>
2026-08-27 15:37:58 +0100

    fix allowed hosts for render

--- appraisal_system/settings.py
+++ appraisal_system/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
-    'https://web-production-cea8c.up.railway.app',  
+    'https://appraisal-system-bu2e.onrender.com',  
 ]
 # Application definition
 
